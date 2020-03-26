6e678dadaa833f4069cc5d377b2417fa291ea0e8
金国群 <guoqun.jin@hotmail.com>
2020-03-26 17:42:56 +0800

    response['Access-Control-Allow-Credentials'] = 'true'

--- car/middlewares.py
+++ car/middlewares.py
@@ -8,5 +8,6 @@ class MiddlewareHead(MiddlewareMixin):
         if request:
             response['Access-Control-Allow-Origin'] = '*'
             response['Access-Control-Allow-Headers'] = '*'
+            response['Access-Control-Allow-Credentials'] = 'true'
             response['X-Frame-Options'] = '*'
         return response
